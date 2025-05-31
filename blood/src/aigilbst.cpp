fb6d4c3423365d8f7a5fda80f7f4bf09d307cc63
nukeykt <alexeytf2@gmail.com>
2025-06-01 02:00:41 +0900

    fix unused gillbeast state

--- blood/src/aigilbst.cpp
+++ blood/src/aigilbst.cpp
@@ -57,7 +57,7 @@ AISTATE gillBeastSwimBite = { 7, nGillBiteClient, 0, NULL, NULL, thinkSwimChase,
 AISTATE gillBeastSwimRecoil = { 5, -1, 0, NULL, NULL, NULL, &gillBeastSwimDodge };
 AISTATE gillBeast13A138 = { 10, -1, 120, NULL, func_6CD74, thinkSwimChase, &gillBeastSwimChase };
 AISTATE gillBeast13A154 = { 10, -1, 0, NULL, func_6D03C, thinkSwimChase, &gillBeastSwimChase };
-AISTATE gillBeast13A170 = { 10, -1, 120, NULL, NULL, aiMoveTurn, &gillBeastSwimChase };
+AISTATE gillBeast13A170 = { 10, -1, 120, NULL, aiMoveTurn, NULL, &gillBeastSwimChase };
 
 static void GillBiteSeqCallback(int, int nXSprite)
 {
